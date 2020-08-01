28fc3098d836eeb9a29bd6d846dc406f096a3428
Sabrina Colman <sabrina-rct@hotmail.com>
2020-07-31 21:42:01 -0300

    finish exercise-one-bis

---
+++ exercise-one-bis/src/ar/com/ada/online/second/exerciseonebis/Main.java
@@ -0,0 +1,38 @@
+package ar.com.ada.online.second.exerciseonebis;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // declaro las variables y titulo el programa
+
+        System.out.println("Programa para determinar si una persona puede votar con base en su edad\n" +
+                "en las próximas elecciones");
+
+        int age;
+        String result;
+
+        //metodo para ingresar datos por keyboard
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Ingrese edad:");
+        age = keyboard.nextInt();
+
+        //estructura de control
+
+        if (age >= 18) {
+            //resultado true
+            result = "Puede Votar";
+
+        } else{
+                //resultado false
+                result = "No puede votar";
+
+            }
+
+        System.out.println("La persona:" + result);
+
+
+    }
+}
